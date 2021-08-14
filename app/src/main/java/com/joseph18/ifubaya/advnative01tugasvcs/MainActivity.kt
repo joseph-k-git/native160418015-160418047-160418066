a4d3d47425ea1a3b4ae3247098d3f0b2cfbe11b0
ASUS <hermanlau89@gmail.com>
2021-08-14 09:57:59 +0800

    halo toast dari albert

--- app/src/main/java/com/joseph18/ifubaya/advnative01tugasvcs/MainActivity.kt
+++ app/src/main/java/com/joseph18/ifubaya/advnative01tugasvcs/MainActivity.kt
@@ -2,10 +2,12 @@ package com.joseph18.ifubaya.advnative01tugasvcs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Albert 160418047 hadir", Toast.LENGTH_SHORT).show()
     }
 }
