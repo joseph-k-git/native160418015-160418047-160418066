6b696d34a33e0dae6024b71da74b0f0404f3654c
sameh4d4 <71703749+sameh4d4@users.noreply.github.com>
2021-08-16 06:24:07 +0700

    Toast Nicky

--- app/src/main/java/com/joseph18/ifubaya/advnative01tugasvcs/MainActivity.kt
+++ app/src/main/java/com/joseph18/ifubaya/advnative01tugasvcs/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val n1:String="Nicky"
+    val nrp1:String="160418066"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, "Albert 160418047 hadir", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "$n1 $nrp1 hadir", Toast.LENGTH_SHORT).show()
     }
 }
